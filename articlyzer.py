58ce238753237aafc27ee6eeb3358135399f9c68
Chris Pratt <cwpratt@gmail.com>
2019-05-13 11:38:44 -0400

    added a gitignore, completed word extractor, added json files

--- articlyzer.py
+++ articlyzer.py
@@ -23,20 +23,19 @@ import re
 # Make an object with like word count, unique etc as properties of the object???
 # SET UP SERVER TO GET THIS DATA EVERY DAY
 # INCORPORATE DATE INTO THINGS TO GET HISTORIC AND OVER TIME AVERAGES ETC
-
-with open('slate_articles.json') as f:
-	raw_slate = json.load(f)
-
-
 # Attempts to get all the induvidual words
 # then returns list of each word in file
 
-def word_extractor(f):
+
+############ NEED TO TAKE OUT ', AND COUNTING DUPLICATES//WHY ARE THERED DUPES'
+###MAYBE JUST WRITE A COUNTER UR SELF GOD DAMNIT
+
+def word_extractor(articles_from_site): #takes json file and returns a list of unique words (no spec)
 	words = []
 	word_list = []
 	titles = []
-	with open(f) as outfile:
-		raw_file = json.load(outfile)
+	with open(articles_from_site) as file:
+		raw_file = json.load(file)
 		less_raw_file = raw_file['articles']
 		for days in less_raw_file:
 			titles.append(days['titles']) # this is just the article arrays
@@ -45,13 +44,35 @@ def word_extractor(f):
 						word_list.append(title.split())
 		for title_chunk in word_list:
 			for i in title_chunk:
-				words.append(i)
+				words.append(re.sub(r'\W+', '',i))
 	return words
 
-def word_counter(word_list):
+def word_counter(word_list): #returns a dict that has a count of each word
 	count = Counter(word_list)
-	return count
+	return dict(count)
 	
+def summary_stats(word_count): 
+# gives diagnostics of given word_count
+# do we want an i/o where like you request a type of graph and it gives it 
+# to you and then asks if you want more and then gives quit option
+	
+
+
+	#def cmc_histo(card_list):
+ #    fig, ax = plt.subplots()
+ #    cost=[]
+ #    count=[]
+ #    histo_tuples= cmc_counter(card_list)
+ #    for cmc_tuple in histo_tuples:
+ #        cost.append(cmc_tuple[0])
+ #        count.append(cmc_tuple[1])
+ #    ax.bar(cost, count, color='salmon')
+ #    ax.set_title('Count of Converted Mana Cost')
+ #    ax.set_xlabel('Mana Costs')
+ #    ax.set_ylabel('Count')
+ #    plt.show()
+#def article_generator(hmm idk yet):
+
 # Grammer stuff?
 
 # Article Generatorrrrrrrrr fun 
@@ -60,5 +81,25 @@ def word_counter(word_list):
 
 
 
-word_count = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
